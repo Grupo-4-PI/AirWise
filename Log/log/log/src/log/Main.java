c01e73ce02c3d3239d13c35fbb0e544d7b39251c
Vinicius Ventura Cavalcante <vinicius.cavalcante@sptech.school>
2025-09-09 14:32:34 -0300

    UPTADE: atualizacoes log

--- Log/log/log/src/log/Main.java
+++ Log/log/log/src/log/Main.java
@@ -1,49 +1,27 @@
 package log;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
-    public static void main(String[] args) {
 
-        System.out.print("Iniciando tratamento de dados - Arquivo XPTO.\n");
+    public static void main(String[] args) throws InterruptedException {
 
-        String data = "2025/09/16";
-        Integer contador_linha = 1;
+        DateTimeFormatter formatadorDeTempo = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
 
-        Integer hora = 0;
-        Integer minuto = 0;
-        Integer segundo = 0;
-        Integer milissegundo = 0;
+        String horaAtualFormatada = LocalTime.now().format(formatadorDeTempo);
+        System.out.println("[INFO] %s - Iniciando tratamento de dados - Arquivo XPTO.".formatted(horaAtualFormatada));
 
-        while (contador_linha <= 1000) {
+        int totalLinhas = 100;
 
-            milissegundo += 26;
-            segundo += 15;
-            minuto += 1;
-
-            if (milissegundo >= 60) {
-                milissegundo -= 60;
-                segundo += 1;
-            }
-
-            if (segundo >= 60) {
-                segundo -= 60;
-                minuto += 1;
-            }
-
-            if (minuto >= 60) {
-                minuto -= 60;
-                hora += 1;
-            }
-
-            System.out.print("Linha %d %s - %02d:%02d:%02d:%02d;\n".formatted(contador_linha, data, hora, minuto, segundo, milissegundo));
-
-            contador_linha += 10;
+        for (int linhaAtual = 1; linhaAtual <= totalLinhas; linhaAtual += 10) {
+            TimeUnit.MILLISECONDS.sleep(150);
+            horaAtualFormatada = LocalTime.now().format(formatadorDeTempo);
+            System.out.println("[INFO] " + horaAtualFormatada + " - Processando linha " + linhaAtual + "...");
         }
 
-        minuto += 1;
-        segundo += 15;
-
-        System.out.print("Linha 1000 %s - %02d:%02d:%02d:%02d;\n".formatted(data, hora, minuto, segundo, milissegundo));
-
-        System.out.print("\nTratamento de dados concluído.");
+        horaAtualFormatada = LocalTime.now().format(formatadorDeTempo);
+        System.out.println("[INFO] " + horaAtualFormatada + " - Tratamento de dados concluído.");
     }
 }
