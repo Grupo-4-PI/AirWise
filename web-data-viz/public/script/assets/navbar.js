7d68b0f4035aa2b9d2b7cde42f3bd95d4b1225f6
FernandoRDev457 <fernando.gutierrez@sptech.school>
2025-09-15 20:27:28 -0300

    update: Atualizando conteudo, icones e imagens

--- web-data-viz/public/script/assets/navbar.js
+++ web-data-viz/public/script/assets/navbar.js
@@ -1,6 +1,15 @@
 const menuToggle = document.querySelector('.menu-toggle');
 const nav = document.querySelector('nav');
+const navLinks = document.querySelectorAll('.nav-links a');
 
 menuToggle.addEventListener('click', () => {
   nav.classList.toggle('open');
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    if (nav.classList.contains('open')) {
+      nav.classList.remove('open');
+    }
+  });
 });
