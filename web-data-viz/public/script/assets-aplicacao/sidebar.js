14e27e0dc9acde7b5d0f44a97653ad1f84543290
FernandoRDev457 <fernando.gutierrez@sptech.school>
2025-10-17 20:03:50 -0300

    style: Atualizando kpis da dash inicial e implementando função de collapsed na main das dash

--- web-data-viz/public/script/assets-aplicacao/sidebar.js
+++ web-data-viz/public/script/assets-aplicacao/sidebar.js
@@ -1,6 +1,8 @@
 const sidebar = document.querySelector('.sidebar');
+const main = document.querySelector('main');
 const toggleBtn = document.querySelector('.toggle-btn');
 
 toggleBtn && toggleBtn.addEventListener('click', () => {
     sidebar.classList.toggle('collapsed');
+    main.classList.toggle('collapsed')
 });
