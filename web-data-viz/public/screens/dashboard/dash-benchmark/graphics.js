8fc6c5321a1fd9c290f92fa995e018076807ac8b
FernandoRDev457 <fernando.gutierrez@sptech.school>
2025-10-28 21:35:54 -0300

    update: Implementando dados reais da Tela de Comparativo com o Mercado

--- web-data-viz/public/screens/dashboard/dash-benchmark/graphics.js
+++ web-data-viz/public/screens/dashboard/dash-benchmark/graphics.js
@@ -1,17 +1,17 @@
 // Dados do gráfico "Melhores Desempenhos"
-const melhoresLabels = ["Cobrança", "Contrato / Oferta", "Privacidade"];
+const melhoresLabels = ["Cobrança", "Contrato / Oferta", "Privacidade", "Atendimento SAC"];
 const melhoresData = {
     labels: melhoresLabels,
     datasets: [
         {
             label: 'Empresa',
-            data: [4.27, 4.25, 4.25],
+            data: [4.27, 4.25, 4.25, 4.13],
             backgroundColor: '#1f77b4'
         },
         {
             label: 'Mercado',
-            data: [3.83, 3.81, 3.67],
-            backgroundColor: '#ff7f0e'
+            data: [3.83, 3.81, 3.67, 3.77],
+            backgroundColor: '#ae00d5ff '
         }
     ]
 };
@@ -42,19 +42,19 @@ new Chart(
 );
 
 // Dados do gráfico "Piores Desempenhos"
-const pioresLabels = ["Saúde e Segu.", "Entrega do Prod.", "Vício de Quali."];
+const pioresLabels = ["Informação","Saúde e Segu.", "Entrega do Prod.", "Vício de Quali.",];
 const pioresData = {
     labels: pioresLabels,
     datasets: [
         {
             label: 'Empresa',
-            data: [3.61, 3.55, 3.51],
+            data: [4.07, 3.61, 3.55, 3.51],
             backgroundColor: '#1f77b4'
         },
         {
             label: 'Mercado',
-            data: [3.43, 3.51, 3.44],
-            backgroundColor: '#ff7f0e'
+            data: [3.72, 3.43, 3.51, 3.44],
+            backgroundColor: '#ae00d5ff'
         }
     ]
 };
