3f4e51bf88cc89bdffdc5cde6757704a4ffed39d
joaovictoradsb <joao.lsilva@sptech.school>
2025-11-15 18:20:16 -0300

    feat: consumindo dados visão geral

--- web-data-viz/src/controllers/visaoGeralController.js
+++ web-data-viz/src/controllers/visaoGeralController.js
@@ -3,35 +3,45 @@ var visaoGeralModel = require("../models/visaoGeralModel");
 function getDataVisaoGeral(req, res) {
   var nomeDaEmpresa = req.query.nomeEmpresaServer;
 
-  if (nomeDaEmpresa == undefined) {
-    res.status(400).send("Nome da Empresa desconhecido");
-  } else {
-    visaoGeralModel
-      .getDataKPIsVisaoGeral(nomeDaEmpresa)
-      .then(function (resultadoKPI) {
-        visaoGeralModel
-          .graficoNotaMediaVisaoGeral(nomeDaEmpresa)
-          .then((resultadoGrafico1) => {
-            visaoGeralModel
-              .graficoEstadoNotaMedia(nomeDaEmpresa)
-              .then((resultadoGrafico2) => {
-                res.json({
-                  kpis: resultadoKPI,
-                  grafico1: resultadoGrafico1,
-                  grafico2: resultadoGrafico2,
-                });
-              });
-          });
-      })
-      .catch(function (erro) {
-        console.log(erro);
-        console.log(
-          "\nNão foi possivel encontrar os dados: ",
-          erro.sqlMessage
-        );
-        res.status(500).json(erro.sqlMessage);
-      });
+  if (!nomeDaEmpresa) {
+    return res.status(400).send("Nome da Empresa desconhecido");
   }
+
+  // Chamadas separadas, todas independentes
+  const chamadaKPI = visaoGeralModel
+    .getDataKPIsVisaoGeral(nomeDaEmpresa)
+    .catch((erro) => {
+      console.log("Erro KPI:", erro);
+      return null;
+    });
+
+  const chamadaGrafico1 = visaoGeralModel
+    .graficoNotaMediaVisaoGeral(nomeDaEmpresa)
+    .catch((erro) => {
+      console.log("Erro Grafico 1:", erro);
+      return null;
+    });
+
+  const chamadaGrafico2 = visaoGeralModel
+    .graficoEstadoNotaMedia(nomeDaEmpresa)
+    .catch((erro) => {
+      console.log("Erro Grafico 2:", erro);
+      return null;
+    });
+
+  // Agora junta todas as respostas
+  Promise.all([chamadaKPI, chamadaGrafico1, chamadaGrafico2])
+    .then(([resultadoKPI, resultadoGrafico1, resultadoGrafico2]) => {
+      res.json({
+        kpis: resultadoKPI,
+        grafico1: resultadoGrafico1,
+        grafico2: resultadoGrafico2,
+      });
+    })
+    .catch((erro) => {
+      console.log("Erro final:", erro);
+      res.status(500).json("Erro inesperado ao processar dados");
+    });
 }
 
 module.exports = {
